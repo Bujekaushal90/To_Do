8eeb5b5611c6cf8cb442258c166f0f7898fbbe90
Bujekaushal90 <bujekaushal5@gmail.com>
2025-11-14 23:14:02 -0800

    todo app final

--- ToDo_App/views.py
+++ ToDo_App/views.py
@@ -4,8 +4,10 @@ from ToDo.models import Task
 
 def home(request):
     task = Task.objects.filter(is_completes = False)
+    completed_task = Task.objects.filter(is_completes = True)
     context = {
-        'task' : task
+        'task' : task,
+        'completed_task' : completed_task
     }
     return render(request, 'home.html' , context)
     
